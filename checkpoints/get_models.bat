5dea7e82b4680a31026c88f89d007ea830b5120b
Nick Fox-Gieg <nick@fox-gieg.com>
2023-10-22 21:42:22 -0400

    shell

--- checkpoints/get_models.bat
+++ checkpoints/get_models.bat
@@ -1,7 +1,6 @@
 @echo off
 
 cd %~dp0
-cd model
 powershell -Command "Invoke-WebRequest https://fox-gieg.com/patches/github/n1ckfg/latk_ml_004/checkpoints/model.zip -OutFile model.zip"
 powershell Expand-Archive model.zip -DestinationPath .
 del model.zip
--- onnx/get_models.bat
+++ onnx/get_models.bat
@@ -1,7 +1,7 @@
 @echo off
 
 cd %~dp0
-cd model
+
 powershell -Command "Invoke-WebRequest https://fox-gieg.com/patches/github/n1ckfg/latk_ml_004/onnx/model.zip -OutFile model.zip"
 powershell Expand-Archive model.zip -DestinationPath .
 del model.zip
